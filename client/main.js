6ec7d7654d12aab878f5650b6488a3d2ff4ec63c
Tommyluo <tluo12321@yahoo.com>
2015-03-11 15:10:14 -0700

    fixed bugs, cleaned up some code

--- client/main.js
+++ client/main.js
@@ -1,133 +1,5 @@
 /* global _ */
 'use strict';
-// $(document).ready();
-// $('#game').find('td').click(function() {
-//   var images = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg',
-//   'images/4.jpg', 'images/5.jpg', 'images/6.jpg', 'images/7.jpg',
-//   'images/8.jpg'];
-//
-//   function shuffle(img) {
-//     for (var x, y, i = img.length; i; x = Math.floor(Math.random() * i), y = img[--i], img[i] = img[x], img[x] = y) {
-//     }
-//     return img;
-//   }
-//
-//   var image = $('#td').find('img');
-//     image = shuffle(images);
-//
-//   console.log(images);
-//   var id = $(this).attr('id');
-//   console.log(id);
-//
-//   if (id === 'td1') {
-//     if (isBlank('td2'))
-//     swap(id, 'td2');
-//     else if (isBlank('td4'))
-//     swap(id, 'td4');
-//   }
-//
-//   if (id === 'td2')
-//   {
-//     if (isBlank('td1'))
-//     swap(id, 'td1');
-//     else if (isBlank('td3'))
-//     swap(id, 'td3');
-//     else if (isBlank('td5'))
-//     swap(id, 'td5');
-//   }
-//
-//   if (id === 'td3')
-//   {
-//     if (isBlank('td2'))
-//     swap(id,'td2');
-//     else if (isBlank('td6'))
-//     swap(id,'td6');
-//   }
-//
-//   if (id === 'td4')
-//   {
-//     if (isBlank('td1'))
-//     swap(id, 'td1');
-//     else if (isBlank('td5'))
-//     swap(id, 'td5');
-//     else if (isBlank('td7'))
-//     swap(id, 'td7');
-//   }
-//
-//   if (id === 'td5')
-//   {
-//     if (isBlank('td2'))
-//     swap(id, 'td2');
-//     else if (isBlank('td4'))
-//     swap(id, 'td4');
-//     else if (isBlank('td6'))
-//     swap(id, 'td6');
-//     else if (isBlank('td8'))
-//     swap(id, 'td8');
-//   }
-//
-//   if (id === 'td6')
-//   {
-//     if (isBlank('td9'))
-//     swap(id, 'td9');
-//     else if (isBlank('td5'))
-//     swap(id, 'td5');
-//     else if (isBlank('td3'))
-//     swap(id, 'td3');
-//   }
-//
-//   if (id === 'td7')
-//   {
-//     if (isBlank('td4'))
-//     swap(id, 'td4');
-//     else if (isBlank('td8'))
-//     swap(id, 'td8');
-//   }
-//
-//   if (id === 'td8')
-//   {
-//     if (isBlank('td7'))
-//     swap(id, 'td7');
-//     else if (isBlank('td5'))
-//     swap(id, 'td5');
-//     else if (isBlank('td9'))
-//     swap(id, 'td9');
-//   }
-//
-//   if (id === 'td9')
-//   {
-//     if (isBlank('td6'))
-//     swap(id, 'td6');
-//     else if (isBlank('td8'))
-//     swap(id, 'td8');
-//   }
-//
-//
-// });
-//
-// function isBlank(id) {
-//   var empty = false;
-//
-//   var num = $('#' + id).attr('data-num');
-//   if (num == 0) {
-//     empty = true;
-//   }
-//   return empty;
-// }
-//
-// function swap(id1, id2) {
-//   var $td1 = $('#' + id1);
-//   var $td2 = $('#' + id2);
-//
-//   var num1 = $('#' + id1).attr('data-num');
-//   var num2 = $('#' + id2).attr('data-num');
-//
-//   var html1 = $td1.html();
-//   var html2 = $td2.html();
-//
-//   $td1.html(html2).attr('data-num', num2);
-//   $td2.html(html1).attr('data-num', num1);
-// }
 
 $(function($, window) {
 
@@ -179,6 +51,8 @@ $(function($, window) {
       $('#box').html($wraps);
       console.log($wraps);
 
+      $('.image').last().css('visibility', 'hidden');
+      
       $('.tile').first().attr({'id': 'td1', 'data-num': '1'});
       $('.tile').first().next().attr({'id': 'td2', 'data-num': '2'});
       $('.tile').first().next().next().attr({'id': 'td3', 'data-num': '3'});
@@ -189,7 +63,6 @@ $(function($, window) {
       $('.tile').last().prev().attr({'id': 'td8', 'data-num': '8'});
       $('.tile').last().attr({'id': 'td9', 'data-num': '0'});
 
-      $('.image').last().css('visibility', 'hidden');
 
       $('.tile').click(function() {
         var id = $(this).attr('id');
